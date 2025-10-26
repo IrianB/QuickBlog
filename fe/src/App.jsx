28ea18d5d92273c45eadf3e8e5ae3f6f31d44df5
Mirabed <irianharyllbecera@gmail.com>
2025-10-26 18:21:14 +0800

    add routing pages

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Blog from './pages/Blog'
 
 const App = () => {
   return (
-    <div>App</div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/blog" element={<Blog />} />
+    </Routes>
   )
 }
 
