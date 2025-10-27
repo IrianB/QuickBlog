9bd3d1cecb6d902a98e58708b6ee9de49ea8e2b8
Mirabed <irianharyllbecera@gmail.com>
2025-10-28 00:03:22 +0800

    check blog_data id from assets

--- fe/src/pages/Blog.jsx
+++ fe/src/pages/Blog.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { blog_data } from '../assets/assets'
+import { useParams } from 'react-router-dom'
+
+
+
 
 const Blog = () => {
-  return (
+
+  const {id} = useParams()
+
+
+  const [data, setData] = useState(null)
+
+  const fetchData = async () => {
+    const res = blog_data.find(item => item._id === id)
+    setData(res)
+  }
+
+  console.log(data)
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+
+  return data ? (
     <div>Blog</div>
+  ) : (
+    <div>Loading...</div>
   )
 }
 
