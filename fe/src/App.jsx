6b1a514a38de5e90b7262727caf76a329550b146
Mirabed <irianharyllbecera@gmail.com>
2025-10-29 02:27:20 +0800

    setup admin routes

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -2,12 +2,23 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Blog from './pages/Blog'
+import Layout from './pages/admin/Layout'
+import Dashboard from './pages/admin/Dashboard'
+import ListBlog from './pages/admin/ListBlog'
+import AddBlog from './pages/admin/AddBlog'
+import Comments from './pages/admin/Comments'
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/blog/:id" element={<Blog />} />
+      <Route path='/admin' element={<Layout />}>
+        <Route index element={<Dashboard />} />
+        <Route path='list' element={<ListBlog />} />
+        <Route path='add' element={<AddBlog />} />
+        <Route path='comments' element={<Comments />} />  
+      </Route>
     </Routes>
   )
 }
